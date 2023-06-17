156d08b30e21c560144c662c3ff9e8eb5c11b935
Mindy Medeiros <mmedei6@wgu.edu>
2023-06-17 13:57:53 -0400

    Added floating point version of temp conversion program

---
+++ temp_conversion_2.c
@@ -0,0 +1,23 @@
+/* Second provided example of temperature conversion table - changes include declaring far and celsius to be float, and the formula being written in a more "natural" way. */
+
+#include <stdio.h>
+
+/* print Farenheit-Celsius table
+    for fahr = 0, 20, ..., 300; floating point version */
+
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("3.0%f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
