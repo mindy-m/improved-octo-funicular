f40663077c314e36a3105b4b30de4839ba38a79c
Mindy Medeiros <mmedei6@wgu.edu>
2023-06-17 13:58:32 -0400

    Added prompt for exercise 1.3 with provided program

---
+++ exercise_1_3.c
@@ -0,0 +1,23 @@
+/* Modify the temperature conversion program to print a heading above the table. */
+
+#include <stdio.h>
+
+/* print Farenheit-Celsius table
+    for fahr = 0, 20, ..., 300; floating point version */
+
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("3.0%f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
