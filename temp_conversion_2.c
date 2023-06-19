6bd419cfd568116a643c55a2501267f8d7b9207e
Mindy Medeiros <mmedei6@wgu.edu>
2023-06-18 22:13:12 -0400

    Fixed typo: misplaced %

--- temp_conversion_2.c
+++ temp_conversion_2.c
@@ -17,7 +17,7 @@ main()
     fahr = lower;
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
-        printf("3.0%f %6.1f\n", fahr, celsius);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
